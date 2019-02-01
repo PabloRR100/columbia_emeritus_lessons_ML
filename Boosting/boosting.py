7d771052b865b29ddbc1e308a9bff3377ab83dad
PabloRR100 <pabloruizruiz10@gmail.com>
2019-02-01 11:02:39 +0100

    moving to real dataset

--- Boosting/boosting.py
+++ Boosting/boosting.py
@@ -481,3 +481,194 @@ n = 10
 dw = default_weights(n)
 print(dw) #--> np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
 
+
+def boot_strap_selection(X, y, weights):
+    """
+    Create and return a boot-strapped sample of the given data,
+    According to the provided weights.
+    
+    Positional Arguments:
+        X -- a numpy array, corresponding to the matrix of x-observations
+        y -- a numpy array, corresponding to a vector of y-labels
+            All either 0 or 1
+        weights -- a numpy array, corresponding to the rate at which the observations
+            should be sampled for the boot-strap. 
+    """
+    # Take random sample of indicies, with replacement
+    bss_indicies = np.random.choice(range(len(y)), size = len(y), p = weights)
+    
+    # Subset arrays with indicies
+    return X[bss_indicies,:], y[bss_indicies]
+
+### Example of use
+X = np.array([[1,1],[2,2],[3,3],[4,4],[5,5]])
+y = np.array([1,0,1,0,1])
+weights = np.array([.35,.1,.1,.35,.1])
+
+print(boot_strap_selection(X,y, weights))    
+
+
+def calc_epsilon(y_true, y_pred, weights):
+    
+    """
+    Calculate the value of epsilon, given the above equation 
+    
+    Positional Arguments:
+        y_true -- An np.array of 1's and 0's corresponding to whether each observation is
+            a member of class 1 or class 2
+        y_pred -- An np.array of 1's and 0's corresponding to whether each observation was
+            predicted to be a member of class 1 or class 2
+        weights -- An np.array of floats corresponding to each observation's weight. 
+            All the weights will sum up to 1.
+        
+    Assumptions:
+        Assume both the true labels and the predictions are both all 0's and 1's.
+    """
+    return float(np.dot(weights, y_pred == y_true))
+    
+y_true = np.array([1,0,1,1,0])
+y_pred = np.array([0,0,0,1,0])
+weights = np.array([.4,.4,.1,.05,.05])
+
+ep = calc_epsilon(y_true, y_pred, weights)
+
+print(ep) # --> .5
+
+
+
+def calc_alpha(epsilon):
+    """
+    Calculate the alpha value given the epsilon observed from a model
+    
+    Positional Argument:
+        epsilon -- The epsilon value calculated from a particular model
+    """    
+    return float(0.5 * np.log((1-epsilon)/epsilon))
+
+ep = .4
+alpha = calc_alpha(ep)
+print(alpha) # --> 0.2027325540540821
+
+
+def update_weights(weights, alpha, y_true, y_pred):
+    """
+    Create an updated vector of weights according to the above equations    
+    Positional Arguments:
+        weights -- a 1-d numpy array of positive floats, corresponding to 
+            observation weights
+        alpha -- a positive float
+        y_true -- a 1-d numpy array of true labels, all 0s and 1s
+        y_pred -- a 1-d numpy array of labels predicted by the last model;
+    """
+    def change_labels(arr):
+        for i,a in enumerate(arr):
+            if a == 0:
+                arr[i] = -1
+        return arr        
+            
+    y_true, y_pred = change_labels(y_true), change_labels(y_pred)
+    w_hat = weights * np.exp(-alpha * y_true * y_pred)
+    return np.array([w_hat / sum(w_hat)])
+
+y_true = np.array([1,0,1,1,0])
+y_pred = np.array([0,0,1,1,1])
+weights = np.array([.4,.4,.1,.05,.05])
+alpha = 0.10033534773107562
+
+print(update_weights(weights, alpha, y_true, y_pred))
+#-->np.array([0.44444444 0.36363636 0.09090909 0.04545455 0.05555556])
+
+
+def predict(X, est_dict):
+    """
+    Create a np.array list of predictions for all of the observations in x,
+    according to the above equation.
+    
+    Positional Arguments:
+        X -- a 2-d numpy array of X observations. Features in columns, 
+            observations in rows.
+        est_dict -- a dictionary consists of keys 0 through n with tuples as values
+            The tuples will be (<mod>, alpha), where alpha is a float, and 
+            <mod> is a sklearn DecisionTreeClassifier
+
+    Assumptions:
+        The models in the `est-dict` tuple will return 0s and 1s.
+            HOWEVER, the prediction equation depends upon predictions
+            of -1s and 1s.
+            FINALLY, the returned predictions should be 0s and 1s.            
+    """
+    
+    def sign(arr):
+        for i,a in enumerate(arr):
+            if a >= 0:
+                arr[i] = 1
+            else:
+                arr[i] = 0
+        return arr
+        
+    def change_labels(arr, l1, l2):
+        for i,a in enumerate(arr):
+            if a == l1:
+                arr[i] = l2
+        return arr        
+    
+    total = 0    
+    for i, (tree, alpha) in enumerate(est_dict.values()):
+        total += alpha * change_labels(tree.predict(X), 0, -1)
+        
+    return np.array([sign(total)])
+
+### Our example dataset, inspired from lecture
+pts = [[.5, 3,1],[1,2,1],[3,.5,0],[2,3,0],[3,4,1],
+ [3.5,2.5,0],[3.6,4.7,1],[4,4.2,1],[4.5,2,0],[4.7,4.5,0]]
+
+df = pd.DataFrame(pts, columns = ['x','y','classification'])
+
+### split out X and labels
+X = df[['x','y']]
+y = df['classification']
+### Split data in half
+X1 = X.iloc[:len(X.index)//2, :]
+X2 = X.iloc[len(X.index)//2:, :]
+
+y1 = y[:len(y)//2]
+y2 = y[len(X)//2:]
+
+
+### Fit classifiers to both sets of data, save to dictionary:
+
+### Tree-creator helper function
+def simple_tree():
+    return DecisionTreeClassifier(criterion = 'entropy', max_depth= 1)
+    
+tree_dict = {}
+
+tree1 = simple_tree()
+tree1.fit(X1,y1)
+print("threshold:", tree1.tree_.threshold[0], "feature:", tree1.tree_.feature[0])
+
+### made up alpha, for example
+alpha1 = .6
+tree_dict[1] = (tree1, alpha1)
+
+tree2 = simple_tree()
+tree2.fit(X2,y2)
+print("threshold:", tree2.tree_.threshold[0], "feature:" ,tree2.tree_.feature[0])
+
+### made up alpha, again.
+alpha2 = .35
+tree_dict[2] = (tree2, alpha2)
+
+print(predict(X, tree_dict))
+#--> np.array([1., 1., 0., 0., 0., 0., 0., 0., 0., 0.])
+
+###############################
+### For Further Checking of your function:
+### The sum of predictions from the two models should be:
+
+# If tree2 splits on feature 1:
+# np.array([ 0.25  0.25 -0.95 -0.95 -0.25 -0.95 -0.25 -0.25 -0.95 -0.25])
+
+# If tree2 splits on feature 0:
+# np.array([ 0.95  0.95 -0.25 -0.25 -0.25 -0.25 -0.25 -0.25 -0.95 -0.95])
+###############################
